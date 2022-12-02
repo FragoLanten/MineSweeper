26b6ba4ab0843842684a1dd3af63415bc325ca9d
Fragolanten <rizinvadim@gmail.com>
2022-12-02 19:10:45 +0300

    устранен баг при котором правый щелчок мыши делал клетку снову закрытой. добавлены картинки мины на красном фоне и неправильного флага мины
    удален длинный файл заполнения цифр вокруг мины

--- src/GameContent.java
+++ src/GameContent.java
@@ -27,10 +27,11 @@ public class GameContent extends JPanel {
     final int DRAW_MINE = 9;
     final int DRAW_COVER = 10;
     final int DRAW_MARK = 11;
-    final int DRAW_WRONG_MARK=12;
+    final int DRAW_WRONG_MARK = 12;
+    final int MINE_WITH_RED_BACKGROUND = 8;
 
     int[][] gameMatrix = new int[FIELD_HEIGHT][FIELD_WIDTH];
-    Image[] img = new Image[12];
+    Image[] img = new Image[13];
 
     boolean inGame = true;
 
@@ -59,10 +60,11 @@ public class GameContent extends JPanel {
     }
 
     public void loadImages() {
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 13; i++) {
             var path = "src/images/" + i + ".png";
             img[i] = (new ImageIcon(path)).getImage();
         }
+
 
     }
 
@@ -92,67 +94,15 @@ public class GameContent extends JPanel {
     }
 
     void placeNumbersAroundMines() {
-        int cell;
+
         for (int y = 0; y < FIELD_HEIGHT; y++) {
             for (int x = 0; x < FIELD_WIDTH; x++) {
                 if (gameMatrix[y][x]==COVERED_MINE_CELL) {
-                    if (y==0&&x==0) {
-                        if (gameMatrix[0][1]!=COVERED_MINE_CELL) gameMatrix[0][1]+=1;
-                        if (gameMatrix[1][0]!=COVERED_MINE_CELL) gameMatrix[1][0]+=1;
-                        if (gameMatrix[1][1]!=COVERED_MINE_CELL) gameMatrix[1][1]+=1;
-                    }
-                    else if (y==0&&(x>0)&&(x<FIELD_WIDTH-1)) {
-                        if (gameMatrix[0][x-1]!=COVERED_MINE_CELL) gameMatrix[0][x-1]+=1;
-                        if (gameMatrix[0][x+1]!=COVERED_MINE_CELL) gameMatrix[0][x+1]+=1;
-                        for (int k = x-1; k < x+2; k++) {
-                            if (gameMatrix[y+1][k]!=COVERED_MINE_CELL) gameMatrix[y+1][k]+=1;
-                        }
-                    }
-                    else if (y==0&&x==FIELD_WIDTH - 1) {
-                        if (gameMatrix[0][FIELD_WIDTH-2]!=COVERED_MINE_CELL) gameMatrix[0][FIELD_WIDTH-2]+=1;
-                        if (gameMatrix[1][FIELD_WIDTH-2]!=COVERED_MINE_CELL) gameMatrix[1][FIELD_WIDTH-2]+=1;
-                        if (gameMatrix[1][FIELD_WIDTH-1]!=COVERED_MINE_CELL) gameMatrix[1][FIELD_WIDTH-1]+=1;
-                    }
-
-                    else if (y==FIELD_HEIGHT-1&&x==0) {
-                        if (gameMatrix[FIELD_HEIGHT-2][0]!=COVERED_MINE_CELL) gameMatrix[FIELD_HEIGHT-2][0]+=1;
-                        if (gameMatrix[FIELD_HEIGHT-2][1]!=COVERED_MINE_CELL) gameMatrix[FIELD_HEIGHT-2][1]+=1;
-                        if (gameMatrix[FIELD_HEIGHT-1][1]!=COVERED_MINE_CELL) gameMatrix[FIELD_HEIGHT-1][1]+=1;
-                    }
-                    else if (y==FIELD_HEIGHT-1&&(x>0)&&(x<FIELD_WIDTH-1)) {
-                        if (gameMatrix[FIELD_HEIGHT-1][x-1]!=COVERED_MINE_CELL) gameMatrix[FIELD_HEIGHT-1][x-1]+=1;
-                        if (gameMatrix[FIELD_HEIGHT-1][x+1]!=COVERED_MINE_CELL) gameMatrix[FIELD_HEIGHT-1][x+1]+=1;
-                        for (int k = x-1; k < x+2; k++) {
-                            if (gameMatrix[FIELD_HEIGHT-2][k]!=COVERED_MINE_CELL) gameMatrix[FIELD_HEIGHT-2][k]+=1;
-                        }
-                    }
-
-                    else if (y==FIELD_HEIGHT-1&&x==FIELD_WIDTH-1) {
-                        if (gameMatrix[FIELD_HEIGHT-2][FIELD_WIDTH-1]!=COVERED_MINE_CELL)
-                            gameMatrix[FIELD_HEIGHT-2][FIELD_WIDTH-1]+=1;
-                        if (gameMatrix[FIELD_HEIGHT-2][FIELD_WIDTH-2]!=COVERED_MINE_CELL)
-                            gameMatrix[FIELD_HEIGHT-2][FIELD_WIDTH-2]+=1;
-                        if (gameMatrix[FIELD_HEIGHT-1][FIELD_WIDTH-2]!=COVERED_MINE_CELL)
-                            gameMatrix[FIELD_HEIGHT-1][FIELD_WIDTH-2]+=1;
-                    }
-                    else if (x==0&&(y>0)&&(y<FIELD_HEIGHT-1)) {
-                        if (gameMatrix[y+1][0]!=COVERED_MINE_CELL) gameMatrix[y+1][0]+=1;
-                        if (gameMatrix[y-1][0]!=COVERED_MINE_CELL) gameMatrix[y-1][0]+=1;
-                        for (int k = y-1; k < y+2; k++) {
-                            if (gameMatrix[k][x+1]!=COVERED_MINE_CELL) gameMatrix[k][x+1]+=1;
-                        }
-                    }
-                    else if (x==FIELD_WIDTH-1&&(y>0)&&(y<FIELD_HEIGHT-1)) {
-                        if (gameMatrix[y+1][FIELD_WIDTH-1]!=COVERED_MINE_CELL) gameMatrix[y+1][FIELD_WIDTH-1]+=1;
-                        if (gameMatrix[y-1][FIELD_WIDTH-1]!=COVERED_MINE_CELL) gameMatrix[y-1][FIELD_WIDTH-1]+=1;
-                        for (int k = y-1; k < y+2; k++) {
-                            if (gameMatrix[k][x-1]!=COVERED_MINE_CELL) gameMatrix[k][x-1]+=1;
-                        }
-                    }
-                    else {
-                        for (int j = y-1; j < y+2; j++) {
-                            for (int k = x-1; k < x+2; k++) {
-                                if (gameMatrix[j][k]!=COVERED_MINE_CELL) gameMatrix[j][k]+=1;
+                    for (int i = y-1; i < y+2; i++) {
+                        for (int j = x-1; j < x+2; j++) {
+                            if ((i>=0&&i<FIELD_HEIGHT)&&(j>=0&&j<FIELD_WIDTH)) {
+                                if (gameMatrix[i][j]!=COVERED_MINE_CELL)
+                                    gameMatrix[i][j]+=1;
                             }
                         }
                     }
@@ -202,15 +152,22 @@ public class GameContent extends JPanel {
                 }
 
                 else    {
+
                     if (cell == COVERED_MINE_CELL) {
-                    cell = DRAW_MINE;
-                    } else if (cell == MARKED_MINE_CELL) {
-                    cell = DRAW_MARK;
-                    } else if (cell > COVERED_MINE_CELL) {
-                    cell = DRAW_WRONG_MARK;
-                    } else if (cell > MINE_CELL) {
-                    cell = DRAW_COVER;
+                        cell = DRAW_MINE;
                     }
+                    else if (cell == MARKED_MINE_CELL) {
+                        cell = DRAW_MARK;
+                    }
+
+                    else if (cell > COVERED_MINE_CELL) {
+                        cell = DRAW_WRONG_MARK;
+                    }
+
+                    else if (cell > MINE_CELL) {
+                        cell = DRAW_COVER;
+                    }
+
                 }
 
                 g.drawImage(img[cell], x * BLOCK_SIZE, y * BLOCK_SIZE, this);
@@ -229,13 +186,14 @@ public class GameContent extends JPanel {
             int x = e.getX()/BLOCK_SIZE;
             int y = e.getY()/BLOCK_SIZE;
 
-            if (y<FIELD_WIDTH*BLOCK_SIZE&&x<FIELD_WIDTH*BLOCK_SIZE) {
+            if (y<FIELD_WIDTH*BLOCK_SIZE&&x<FIELD_WIDTH*BLOCK_SIZE&&inGame) {
                 if (e.getButton()==MouseEvent.BUTTON1) {
                     if (gameMatrix[y][x]>MINE_CELL&&gameMatrix[y][x]<MARKED_MINE_CELL) {
                         gameMatrix[y][x]=gameMatrix[y][x]-COVERED_CELL;
                         doRepaint = true;
                     }
-                    if (gameMatrix[y][x]==COVERED_MINE_CELL) {
+                    if (gameMatrix[y][x]==MINE_CELL) {
+                        gameMatrix[y][x]=MINE_WITH_RED_BACKGROUND;
                         inGame = false;
                     }
                     if (gameMatrix[y][x]==EMPTY_CELL) {
@@ -244,10 +202,10 @@ public class GameContent extends JPanel {
                 }
                 else if (e.getButton()==MouseEvent.BUTTON3) {
                     doRepaint = true;
-                    if (gameMatrix[y][x]<=COVERED_MINE_CELL) {
+                    if (gameMatrix[y][x]<=COVERED_MINE_CELL&&gameMatrix[y][x]>MINE_CELL) {
                         gameMatrix[y][x]+=MARKED_CELL;
                     }
-                    else {
+                    else if (gameMatrix[y][x]>COVERED_MINE_CELL) {
                         gameMatrix[y][x]-=MARKED_CELL;
                     }
                 }
